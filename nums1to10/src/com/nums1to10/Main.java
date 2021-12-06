9dcda40c16240ed1d79bc2b21101539f02519540
musa_dv <musa_dv@abv.bg>
2021-12-07 00:12:21 +0200

    add new file

---
+++ nums1to10/src/com/nums1to10/Main.java
@@ -0,0 +1,11 @@
+package com.nums1to10;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
