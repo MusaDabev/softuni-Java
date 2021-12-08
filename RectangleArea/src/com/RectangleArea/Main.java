11183982c2c97cfe319aaa94c81c63eae16c3298
musa_dv <musa_dv@abv.bg>
2021-12-08 19:01:04 +0200

    adding new project

---
+++ RectangleArea/src/com/RectangleArea/Main.java
@@ -0,0 +1,15 @@
+package com.RectangleArea;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        Scanner scanner1 = new Scanner(System.in);
+        int b = Integer.parseInt(scanner.nextLine());
+        int area = a * b;
+        System.out.println(area);
+    }
+}
